28ae1d9924beba3e8525b648622d31d95b05e40c
Taichi Uragami <backpaper0@gmail.com>
2017-01-22 07:18:23 +0900

    Extract about content

---
+++ src/About.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="box">
+    <article class="media">
+      <div class="media-left">
+        <figure class="image is-64x64">
+          <img src="https://www.gravatar.com/avatar/e107c65b007e7abb6b2e53054428fb5a">
+        </figure>
+      </div>
+      <div class="media-content">
+        <div class="content">
+          <p>
+            <a href="https://github.com/backpaper0/cordova-vue-sample">
+              backpaper0/cordova-vue-sample
+            </a>
+          </p>
+          <hr>
+          <p>Use following:</p>
+          <ul>
+            <li><a href="https://cordova.apache.org/">Cordova</a></li>
+            <li><a href="https://jp.vuejs.org/">Vue.js</a></li>
+            <li><a href="https://vuex.vuejs.org/ja/">Vuex</a></li>
+            <li><a href="https://router.vuejs.org/ja/">vue-router 2</a></li>
+            <li><a href="https://github.com/mzabriskie/axios">axios</a></li>
+            <li><a href="http://bulma.io/">Bulma</a></li>
+            <li><a href="http://fontawesome.io/icons/">Font Awesome icons</a></li>
+            <li><a href="https://webpack.js.org/">webpack</a></li>
+            <li><a href="https://www.npmjs.com/">npm</a></li>
+            <li><a href="https://babeljs.io/learn-es2015/">Babel and ES2015</a></li>
+          </ul>
+        </div>
+      </div>
+    </article>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'about',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+</style>
