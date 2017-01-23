ad64319f7e825d796b103f600eaa64a4d587acad
Taichi Uragami <backpaper0@gmail.com>
2017-01-23 20:22:02 +0900

    Polish

--- src/api.js
+++ src/api.js
@@ -1,12 +1,8 @@
 import axios from 'axios'
 
-const searchUsers = ({ q, sort }) => {
-  const executor = resolve => {
-    axios.get('https://api.github.com/search/users', { params: { q, sort }})
-      .then(res => resolve(res.data.items))
-  }
-  return new Promise(executor)
-}
+const searchUsers = ({ q, sort }) =>
+  axios.get('https://api.github.com/search/users', { params: { q, sort }})
+       .then(res => res.data.items)
 
 export default {
   searchUsers
